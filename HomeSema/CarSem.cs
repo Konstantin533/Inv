16671de29cd50194b7d180a7b91742cd65781fab
Konstantin533 <petya.didi@mail.ru>
2023-06-01 18:44:07 +0300

    Summary (required)

---
+++ HomeSema/CarSem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSema
+{
+    public class CarSem // Семафор
+    {
+        static Semaphore sem = new Semaphore(2, 2);
+        int count = 3;
+        public CarSem(int i) 
+        {
+            var carThread = new Thread(Parking);
+            carThread.Name = $"Водитель {i}";
+            carThread.Start();
+        }
+
+        public void Parking()
+        {
+            while(count>0)
+            {
+                sem.WaitOne();
+                Console.WriteLine($"{Thread.CurrentThread.Name} заезжает на парковку");
+                Console.WriteLine($"{Thread.CurrentThread.Name} паркуется");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} выезжает с парковки");
+                sem.Release();
+                count--;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ HomeSema/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace HomeSema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Семафор
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    var driver = new CarSem(i);
+            //}
+
+
+            //Монитор
+            //int x = 0;
+            //object locker = new();
+            
+            //for (int i = 1; i < 6; i++)
+            //{
+            //    Thread myThread = new(Print);
+            //    myThread.Name = $"Поток {i}";
+            //    myThread.Start();
+            //}
+
+            //void Print()
+            //{
+            //    bool acquiredLock = false;
+            //    try
+            //    {
+            //        Monitor.Enter(locker, ref acquiredLock);
+            //        x = 1;
+            //        for (int i = 1; i < 6; i++)
+            //        {
+            //            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            //            x++;
+            //            Thread.Sleep(100);
+            //        }
+            //    }
+            //    finally
+            //    {
+            //        if (acquiredLock) Monitor.Exit(locker);
+            //    }
+            //}
+
+            //Мьютекс
+            int x = 0;
+            Mutex mutexObj = new();
+
+            
+            for (int i = 1; i < 6; i++)
+            {
+                Thread myThread = new(Print);
+                myThread.Name = $"Поток {i}";
+                myThread.Start();
+            }
+
+            void Print()
+            {
+                mutexObj.WaitOne();     
+                x = 1;
+                for (int i = 1; i < 6; i++)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                    x++;
+                    Thread.Sleep(100);
+                }
+                mutexObj.ReleaseMutex();    
+            }
+        }
+    }
+}
